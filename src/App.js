6dc5c7179556fe034f0adc9f43bbc0e5ea699e9f
ACER <apipska.10@gmail.com>
2023-04-15 11:55:15 +0700

    Bug Fixed

--- src/App.js
+++ src/App.js
@@ -5,18 +5,18 @@ import IedCard from './assets/img/1444h.jpg';
 const App = () => {
 
   const canvasPreview = useRef();
-  let context     = null;
+  let context         = null;
 
   useEffect(() => {
     context = canvasPreview.current.getContext("2d");
     const img = new Image();
     img.onload = function () {
       context.drawImage(img, 0, 0);
-      context.scale(0.45, 0.45);
+      context.scale(.45, .45);
       context.font = "bold 50pt hsimplified";
       context.fillStyle = "white";
       context.textAlign = "center";
-      context.fillText("Selamat Hari Raya Idul Fitri!", 1175, 2700);
+      context.fillText("Selamat Hari Raya Idul Fitri!", 540, 1220);
     }
 
     img.src = IedCard;
@@ -25,7 +25,7 @@ const App = () => {
   return (
     <div className="container">
       <div className="content-wrapper">
-        <canvas ref={canvasPreview} className="canvas-preview" width={300} height={600}></canvas>
+        <canvas ref={canvasPreview} className="canvas-preview img-fluid" width={475} height={608}></canvas>
 
         <div className="content">
           <input type="text" className="input-filltext" />
