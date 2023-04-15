1e819ce5080199a84b151c51e03caf57cb3acf4d
ACER <apipska.10@gmail.com>
2023-04-15 12:00:58 +0700

    Bug Fixed

--- src/App.js
+++ src/App.js
@@ -9,9 +9,9 @@ const App = () => {
 
   useEffect(() => {
     context = canvasPreview.current.getContext("2d");
-    const img = new Image();
-    img.onload = function () {
-      context.drawImage(img, 0, 0);
+    const imgOnLoad = new Image();
+    imgOnLoad.onload = function () {
+      context.drawImage(imgOnLoad, 0, 0);
       context.scale(.45, .45);
       context.font = "bold 50pt hsimplified";
       context.fillStyle = "white";
@@ -19,19 +19,19 @@ const App = () => {
       context.fillText("Selamat Hari Raya Idul Fitri!", 540, 1220);
     }
 
-    img.src = IedCard;
+    imgOnLoad.src = IedCard;
   }, [])
 
   return (
     <div className="container">
-      <div className="content-wrapper">
-        <canvas ref={canvasPreview} className="canvas-preview img-fluid" width={475} height={608}></canvas>
-
-        <div className="content">
-          <input type="text" className="input-filltext" />
-          <div className="btn-wrapper">
-            <button className="btn-tampilkan">Tampilkan</button>
-            <button className="btn-download">Download</button>
+      <div className="mx-auto justify-content-center mt-5">
+        <canvas ref={canvasPreview} className="mx-auto d-block img-fluid" width="475" height="608"></canvas>
+        <hr />
+        <div className="col-sm-5 mx-auto">
+          <input type="text" className="input-filltext form-control" placeholder='Masukkan Nama Anda!' />
+          <div className="mt-3">
+            <button className="btn btn-primary">Tampilkan</button>
+            <button className="btn btn-success ms-3">Download</button>
           </div>
         </div>
       </div>
