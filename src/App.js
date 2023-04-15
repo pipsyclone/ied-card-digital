eb11f9d1c7fce45d8b0f45133e24300df2444a0e
ACER <apipska.10@gmail.com>
2023-04-15 11:44:26 +0700

    Bug Fixed

--- src/App.js
+++ src/App.js
@@ -1,25 +1,42 @@
-import logo from './logo.svg';
-import './App.css';
+import { useEffect, useRef } from 'react';
 
-function App() {
+import IedCard from './assets/img/1444h.jpg';
+
+const App = () => {
+
+  const canvasPreview = useRef();
+  let context     = null;
+
+  useEffect(() => {
+    context = canvasPreview.current.getContext("2d");
+    const img = new Image();
+    img.onload = function () {
+      context.drawImage(img, 0, 0);
+      context.scale(0.45, 0.45);
+      context.font = "bold 50pt hsimplified";
+      context.fillStyle = "white";
+      context.textAlign = "center";
+      context.fillText("Selamat Hari Raya Idul Fitri!", 1175, 2700);
+    }
+
+    img.src = IedCard;
+  }, [])
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="container">
+      <div className="content-wrapper">
+        <canvas ref={canvasPreview} className="canvas-preview" width={475} height={630}></canvas>
+
+        <div className="content">
+          <input type="text" className="input-filltext" />
+          <div className="btn-wrapper">
+            <button className="btn-tampilkan">Tampilkan</button>
+            <button className="btn-download">Download</button>
+          </div>
+        </div>
+      </div>
     </div>
-  );
+  )
 }
 
 export default App;
